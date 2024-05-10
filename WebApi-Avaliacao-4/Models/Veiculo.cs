4c6404a3fbf75c5666322bc13b99f980f4d9e572
Anderson <lvaro@DESKTOP-ORAHIVS>
2024-05-10 10:28:53 -0300

    created-models-class

---
+++ WebApi-Avaliacao-4/Models/Veiculo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApi_Avaliacao_4.Models
+{
+    public class Veiculo
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Marca { get; set; }
+
+        [Required]
+        public string Nome { get; set; }
+
+        [Required]
+        public int AnoModelo { get; set; }
+
+        [Required]
+        public DateTime AnoFabricacao { get; set; }
+
+        [Required]
+        public decimal Valor { get; set; }
+
+        public string Opicionais { get; set; }
+    }
+}
