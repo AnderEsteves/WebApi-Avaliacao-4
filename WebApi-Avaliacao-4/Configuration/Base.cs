7da1b26e90df4e18d133c146860091d870400b00
Anderson <lvaro@DESKTOP-ORAHIVS>
2024-05-10 10:27:00 -0300

    created-connection-base-class

---
+++ WebApi-Avaliacao-4/Configuration/Base.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApi_Avaliacao_4.Configuration
+{
+    public class Base
+    {
+        public static string GetConnectionString()
+        {
+
+            return System.Configuration.ConfigurationManager.ConnectionStrings["loja"].ConnectionString;
+
+        }
+    }
+}
